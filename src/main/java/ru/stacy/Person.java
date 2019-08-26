776c54e0ce006c66cb5567238193062f8e6eba17
Sergey Stasko <heathamm23@gmail.com>
2019-08-27 00:46:28 +0300

    Adding class Person

---
+++ src/main/java/ru/stacy/Person.java
@@ -0,0 +1,13 @@
+package ru.stacy;
+
+public class Person extends BaseEntity {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
